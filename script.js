99528ad99b91f73b31b80a684e5637948701addd
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-03-12 21:10:35 +0300

    feat: add portfolio tabs switching solution

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const MENU = document.querySelector('.navigation');
+const TAGS = document.querySelector('.portfolio_tags');
 
 MENU.addEventListener('click', (event) => {
     if (event.target.tagName === 'A') {
@@ -6,3 +7,28 @@ MENU.addEventListener('click', (event) => {
         event.target.parentNode.classList.add("active");
     }
 });
+
+TAGS.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        TAGS.querySelectorAll('button').forEach(elem => elem.classList.remove('tag_selected'));
+        TAGS.querySelectorAll('button').forEach(elem => elem.classList.add('tag_bordered'));
+        event.target.classList.add("tag_selected");
+        randomMixImages();
+    }
+});
+
+function randomMixImages(){
+    let images = document.querySelectorAll('.portfolio__item');
+    let random = Math.round(Math.random() * (images.length - 1));
+    for (let i = 0; i < images.length; i++) {
+        if (random + i >= images.length) {
+            console.log(random + i - images.length + 2);
+            images[i].childNodes[1].setAttribute('src','./assets/images/portfolio/image_'+
+                (random + i - images.length + 1) +'.png');
+        }
+        else {
+            images[i].childNodes[1].setAttribute('src','./assets/images/portfolio/image_'+
+                (random + i + 1) +'.png');
+        }
+    }
+};
